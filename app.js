76b07462fb7c49e33d7d9e8e6abee7729fc66d73
selsebil-ben <selsebilben.dev@gmail.com>
2024-08-26 16:20:13 +0200

    Create DB, create new schema, compile it as a model and make a post API test

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./models/user');
 
+const app = express();
+const port = 3001;
+const uri = "mongodb+srv://salsa:salsa@cluster0.mtjwq.mongodb.net/UserManagement?retryWrites=true&w=majority&appName=Cluster0";
 
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+
+// git bash path : cd /c/Users/Selsebil/Desktop/my-github/ExpressMangoDB
+
+mongoose.connect(uri)
+.then( () => {console.log("Database successfuly conencted !")})
+.catch( (Error) => {console.log(Error)});
 
 app.get('/', (req, res) => {
   res.sendFile("./views/home.html", {root : __dirname})
-  console.log("I am  a GET API")
+  console.log("Starting page")
 })
 
+app.post("/adduser", (req, res) => {
+  console.log(req.body);
+  const name = new User(req.body);
+  name.save()
+  .then( () => { res.redirect("/success") })
+  .catch( (er) => {console.log(er)});
+   
+})
+
+app.get('/success', (req, res) => {
+  // res.sendFile("./views/home.html", {root : __dirname})
+  res.send("Username added succefully :D")
+})
 
 app.listen(port, () => {
   
   console.log(`http://localhost:${port}/`)
-})
+});
