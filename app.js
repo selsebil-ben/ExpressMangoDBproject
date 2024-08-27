26dadfccba9708121734793fcb7a8b78f91e7f02
selsebil-ben <selsebilben.dev@gmail.com>
2024-08-27 19:37:26 +0200

    make some operation on DB

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const User = require('./models/user');
-
 const app = express();
 const port = 3001;
 const dbString = process.env.DATABASE_URL;
@@ -10,8 +9,25 @@ const dbString = process.env.DATABASE_URL;
 
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
+app.use(express.static('public')) // public is a folder contains static files like css, js or images
 
 app.set('view engine','ejs');
+
+//-------Auto refrech--------------
+
+const path = require("path");
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer(); 
+liveReloadServer.watch(path.join(__dirname, 'public'));
+const connectLivereload = require("connect-livereload");
+app.use(connectLivereload());
+liveReloadServer.server.once("connection", () => {
+setTimeout(() => {
+liveReloadServer.refresh("/");
+}, 100);
+});
+
+//---------------------------------
 
 
 
@@ -20,10 +36,10 @@ mongoose.connect(dbString)
 .catch( (Error) => {console.log(Error)});
 
 app.get('/', (req, res) => {
-  //res.sendFile("./views/home.html", {root : __dirname})
-  res.render("home", {title: "Home page"})
-  
-})
+
+
+res.render("home", {title: "Home page"})})
+
 
 app.post("/adduser", (req, res) => {
   console.log(req.body);
@@ -36,7 +52,10 @@ app.post("/adduser", (req, res) => {
 
 app.get('/success', (req, res) => {
   
-  res.send("Username added succefully :D")
+  User.find()
+  .then( (result) => { res.render("index", {title: "Confirmation page", uname : result})})
+  .catch( (err) => { console.log(err)})
+    
 })
 
 app.listen(port, () => {
